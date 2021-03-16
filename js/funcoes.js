e3c8a0fa45cdecda01c3d2df5ded9294a0e779f1
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2021-03-16 20:57:46 -0300

    projeto-integrador

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+$("#slider").nivoSlider();
+$("#banner").nivoSlider();
+$("#banner1").nivoSlider();
+$("#banner2").nivoSlider();
+$("#banner3").nivoSlider();
+$("#banner4").nivoSlider();
+$("#banner5").nivoSlider();
